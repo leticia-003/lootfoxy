387f44f9a069ecd715926ef4e418f5f99c1b4a55
Letícia Coelho <leticiamendes2122@gmail.com>
2026-04-06 18:41:54 +0100

    added background image

--- src/login_page_react.jsx
+++ src/login_page_react.jsx
@@ -1,3 +1,11 @@
 export default function LoginPage() {
-  return <div className="w-screen h-screen bg-[#06011B]"></div>;
+  return (
+    <div className="w-screen h-screen bg-[#06011B] flex items-center justify-center">
+      <img 
+        src="/background-image.png" 
+        alt="Background" 
+        className="max-w-2xl max-h-2xl opacity-20 blur-md"
+      />
+    </div>
+  );
 }
